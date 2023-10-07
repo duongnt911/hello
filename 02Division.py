75bebeddf1e6ce6e9a4e50589825c6c2dbc0171f
Nguyen Trieu Duong <147221391+duongnt911@users.noreply.github.com>
2023-10-07 11:39:09 +0700

    Update code from cloud

--- 02Division.py
+++ 02Division.py
@@ -1,5 +1,6 @@
 #Elimate all variables
 print(f"Answer: {int(input('First number: ')) / int(input('Second number: '))}")
 print('Hello, world')
+print('Bye, world')
 print()
 input("Press return to continue ...")
