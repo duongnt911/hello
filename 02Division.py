0c3042fc9ed72eb226d14f2ae39993dcce83a1f9
Nguyen Trieu Duong <147221391+duongnt911@users.noreply.github.com>
2023-10-07 11:41:12 +0700

    Update from cloud 2

--- 02Division.py
+++ 02Division.py
@@ -2,5 +2,6 @@
 print(f"Answer: {int(input('First number: ')) / int(input('Second number: '))}")
 print('Hello, world')
 print('Bye, world')
+print('JK')
 print()
 input("Press return to continue ...")
